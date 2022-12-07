ecffa8881dff67c5330f82c6af2940f66529a58b
Tomasz Leś <szkolenia@tomaszles.pl>
2022-12-07 13:37:45 +0100

    Add files via upload

---
+++ importZawodnicy.sql
@@ -0,0 +1,30 @@
+
+CREATE TABLE zawodnicy(
+	id_zawodnika int NULL,
+	id_trenera int  NULL,
+	imie varchar(255) NULL,
+	nazwisko varchar(255) NULL,
+	kraj varchar(255) NULL,
+	data_ur DATE NULL,
+	wzrost int  NULL,
+	waga int  NULL
+) 
+
+
+insert into Zawodnicy values(1,4,'Marcin','BACHLEDA','POL','1982-09-04',166,56)
+insert into Zawodnicy values(2,4,'Robert','MATEJA','POL','1974-10-05',180,63)
+insert into Zawodnicy values(3,5,'Alexander','HERR','GER','1978-10-04',173,65)
+insert into Zawodnicy values(4,5,'Stephan','HOCKE','GER','1983-10-20',178,59)
+insert into Zawodnicy values(5,5,'Martin','SCHMITT','GER','1978-01-29',181,64)
+insert into Zawodnicy values(6,5,'Michael','UHRMANN','GER','1978-09-09',184,64)
+insert into Zawodnicy values(7,5,'Georg','SPAETH','GER','1981-02-24',187,68)
+insert into Zawodnicy values(8,2,'Matti','HAUTAMAEKI','FIN','1981-07-14',174,57)
+insert into Zawodnicy values(9,2,'Tami','KIURU','FIN','1976-09-13',183,59)
+insert into Zawodnicy values(10,2,'Janne','AHONEN','FIN','1977-05-11',184,67)
+insert into Zawodnicy values(11,1,'Martin','HOELLWARTH','AUT','1974-04-13',182,67)
+insert into Zawodnicy values(12,1,'Thomas','MORGENSTERN','AUT','1986-10-30',174,57)
+insert into Zawodnicy values(13,3,'Tommy','INGEBRIGTSEN','NOR','1977-08-08',179,56)
+insert into Zawodnicy values(14,3,'Bjoern-Einar','ROMOEREN','NOR','1981-04-01',182,63)
+insert into Zawodnicy values(15,3,'Roar','LJOEKELSOEY','NOR','1976-05-31',175,62)
+insert into Zawodnicy values(16,1,'Alan','ALBORN','USA','1980-12-13',177,57)
+insert into Zawodnicy values(17,4,'Adam','MA£YSZ','POL','1977-12-03',169,60)
